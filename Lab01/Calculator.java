148011c3fcca00e1d661ee772afac73b386ed534
thanhnguyen1008 <79902590+thanhnguyen1008@users.noreply.github.com>
2024-09-28 12:31:21 +0700

    Add files via upload

---
+++ Lab01/Calculator.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+public class Calculator {
+    public static void main(String[] args) {
+        String str1, str2;
+        str1 = JOptionPane.showInputDialog(null, "Enter the first number: ");
+        str2 = JOptionPane.showInputDialog(null, "Enter the second number: ");
+        double num1 = Double.parseDouble(str1);
+        double num2 = Double.parseDouble(str2);
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+        double quotient = num1 / num2;
+
+        if (num2 == 0) {
+            JOptionPane.showMessageDialog(null, "Division by zero is not allowed.");
+        } else {
+            quotient = num1 / num2;
+            JOptionPane.showMessageDialog(null, "The sum is: " + sum + "\n" + "The difference is: " + difference + "\n" + "The product is: " + product + "\n" + "The quotient is: "+ quotient);
+        }
+    }
+}
