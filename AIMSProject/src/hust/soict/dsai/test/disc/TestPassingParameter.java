572334059965b53d334c6027f52b49db6f68828a
thanhnguyen1008 <79902590+thanhnguyen1008@users.noreply.github.com>
2024-11-29 12:48:56 +0700

    Update TestPassingParameter.java

--- AIMSProject/src/hust/soict/dsai/test/disc/TestPassingParameter.java
+++ AIMSProject/src/hust/soict/dsai/test/disc/TestPassingParameter.java
@@ -1,6 +1,7 @@
+package hust.soict.dsai.test.disc;
+
 import hust.soict.dsai.aims.disc.DigitalVideoDisc;
 
-package hust.soict.dsai.test.disc;
 public class TestPassingParameter {
     public static void main(String[] args) {
         DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
